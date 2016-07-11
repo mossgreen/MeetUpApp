fa1e55cbf97eceb4cdcc9d81c2c1dd2b709e1897
Moss Green <gufeifei@outlook.com>
2016-07-11 21:23:39 +1200

    PM> add-migration createGigTable -force
    added two models

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int  Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; } 
+    }
+}
