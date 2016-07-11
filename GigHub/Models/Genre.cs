4bb0b7d0e0205aeae86acc04c33893ad41777e9b
Moss Green <gufeifei@outlook.com>
2016-07-11 21:35:05 +1200

    PM> add-migration OverrideConventionsForGigsAndGenres

--- GigHub/Models/Genre.cs
+++ GigHub/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace GigHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
     public class Gig
     {
-        public int  Id { get; set; }
+        public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Venue { get; set; }
-        public Genre Genre { get; set; } 
+
+        [Required]
+        [StringLength(255)]
+        public Genre Genre { get; set; }
     }
 }
