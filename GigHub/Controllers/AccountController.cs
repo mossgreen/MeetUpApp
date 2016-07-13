c13f2651025e478c7dc55fb95314075f52cdb4f1
Moss Green <gufeifei@outlook.com>
2016-07-13 21:43:09 +1200

    refactoring. move registerviewmodel to viewmodels folder

--- GigHub/Controllers/AccountController.cs
+++ GigHub/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using GigHub.ViewModels;
 
 namespace GigHub.Controllers
 {
--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -62,29 +62,6 @@ namespace GigHub.Models
         public bool RememberMe { get; set; }
     }
 
-    public class RegisterViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-
-        [Required]
-        [StringLength(100)]
-        public string Name { get; set; }
-    }
-
     public class ResetPasswordViewModel
     {
         [Required]
