a0a463457d508f97d593cc532122ff8bc70812a3
Moss Green <gufeifei@outlook.com>
2016-07-15 17:51:33 +1200

    user need to log in to reach the follow button

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using GigHub.ViewModels;
 using System;
 using System.Data.Entity;
 using System.Linq;
@@ -21,7 +22,13 @@ namespace GigHub.Controllers
                 .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
-            return View(upcomingGigs);
+            var viewModel = new HomeViewModel
+            {
+                UpcommingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated,
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ GigHub/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Gig> UpcommingGigs { get; set; }
+        public bool ShowActions { get; set; }
+
+    }
+}
