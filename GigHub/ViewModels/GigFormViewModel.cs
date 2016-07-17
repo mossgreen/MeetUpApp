c52a62277e93ac83bcc576aa493a9932a46636f9
Moss Green <gufeifei@outlook.com>
2016-07-17 22:41:27 +0800

    use lambda expressions instead of magic strings

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,7 +1,10 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
@@ -30,7 +33,21 @@ namespace GigHub.ViewModels
 
         public string Action
         {
-            get { return (Id != 0) ? "Update" : "Create"; }
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update =
+                    (c => c.Update(this));
+
+                Expression<Func<GigsController, ActionResult>> create =
+                    (c => c.Create(this));
+
+                var action = (Id != 0) ? update : create;
+                var actionName = (action.Body as MethodCallExpression)
+                    .Method
+                    .Name;
+                return actionName;
+
+            }
         }
 
         public DateTime GetDateTime()
