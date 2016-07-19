2d110e91fcae0e0ae805fc1416c6a194c09d9f38
Moss Green <gufeifei@outlook.com>
2016-07-19 21:28:12 +0800

    PM> add-migration AddIsCanceledToGig

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -7,6 +7,8 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
+        public bool IsCanceled { get; set; }
+
         public ApplicationUser Artist { get; set; }
 
         [Required]
