a2b35f5fffdf2b6cfb036e628b7b7d8df5427913
Moss Green <gufeifei@outlook.com>
2016-07-19 22:47:32 +0800

    excluding logically-deleted objects from queries

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -24,7 +24,10 @@ namespace GigHub.Controllers
 
             var userId = User.Identity.GetUserId();
             var gigs = _context.Gigs
-                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Where(g =>
+                g.ArtistId == userId
+                && g.DateTime > DateTime.Now
+                && !g.IsCanceled)
                 .Include(g => g.Genre)
                 .ToList();
 
--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace GigHub.Controllers
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
                 .Include(g => g.Genre)
-                .Where(g => g.DateTime > DateTime.Now);
+                .Where(g => g.DateTime > DateTime.Now && !g.IsCanceled);
 
             var viewModel = new GigsViewModel
             {
