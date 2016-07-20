4d1d52aaf0096590e92c18725851fd789b138c4e
Moss Green <gufeifei@outlook.com>
2016-07-20 20:54:42 +0800

    PM> add-migration AddNotification, with errors
    can not update database

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -10,6 +10,9 @@ namespace GigHub.Models
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
         public DbSet<Following> Followings { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<UserNotification> UserNotifications { get; set; }
+
 
         public ApplicationDbContext()
         //            : base("DefaultConnection", throwIfV1Schema: false)
