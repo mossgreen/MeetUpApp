5155681f8c9ab38cbe9a77edf7ac9cb177ab3b47
Moss Green <gufeifei@outlook.com>
2016-07-23 21:50:39 +0800

    building NotificationsController api

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Notification> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifications;
+        }
+    }
+}
