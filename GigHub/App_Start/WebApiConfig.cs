dd6a4b0a23b56c0c57b5a6caa3510c618c840cdd
Moss Green <gufeifei@outlook.com>
2016-07-23 23:29:53 +0800

    using camel notation
    note the using statement!

--- GigHub/App_Start/WebApiConfig.cs
+++ GigHub/App_Start/WebApiConfig.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace GigHub
@@ -9,6 +8,11 @@ namespace GigHub
     {
         public static void Register(HttpConfiguration config)
         {
+
+            var settings = GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
