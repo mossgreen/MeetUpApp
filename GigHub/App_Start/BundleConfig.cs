ab0e80e6f65552be4de3f3e3fcda520956beb268
Moss Green <gufeifei@outlook.com>
2016-07-24 10:25:00 +0800

    animating Content with CSS
    using animate.css

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animate.css"));
         }
     }
 }
