a99251d2a1e24f5cb7289961647aff15e0342d15
Moss Green <gufeifei@outlook.com>
2016-07-26 13:19:18 +0800

    formatting dates using moment.js

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace GigHub
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore-min.js"));
+                        "~/Scripts/underscore-min.js",
+                        "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
