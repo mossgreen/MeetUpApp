0b92d722feefbe38ec25253c5db0987c8c1441fe
Moss Green <gufeifei@outlook.com>
2016-07-26 16:30:26 +0800

    adding the search form

--- GigHub/ViewModels/GigsViewModel.cs
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -8,5 +8,6 @@ namespace GigHub.ViewModels
         public IEnumerable<Gig> UpcomingGigs { get; set; }
         public bool ShowActions { get; set; }
         public string Heading { get; set; }
+        public string SearchTerm { get; set; }
     }
 }
