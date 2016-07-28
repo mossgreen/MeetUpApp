68d0c696d8ff76ba47ca38b5be7d6128f5730254
Moss Green <gufeifei@outlook.com>
2016-07-28 21:02:16 +0800

    adding HttpDelete method to AttendancesController
    we need both userId and GigId to location the right attendance. find it
    and delete it.

--- GigHub/Controllers/Api/AttendancesController.cs
+++ GigHub/Controllers/Api/AttendancesController.cs
@@ -1,8 +1,8 @@
-using System.Linq;
-using System.Web.Http;
 using GigHub.Dtos;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
 
 namespace GigHub.Controllers.Api
 {
@@ -39,6 +39,23 @@ namespace GigHub.Controllers.Api
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = _context.Attendances
+                .SingleOrDefault(a => a.AttendeeId == userId && a.GigId == id);
+
+            if (attendance == null)
+                return NotFound();
+
+            _context.Attendances.Remove(attendance);
+            _context.SaveChanges();
+
+            return Ok(id);
         }
     }
 }
