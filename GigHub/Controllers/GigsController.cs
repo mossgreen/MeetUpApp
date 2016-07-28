de46bb266ca2393364af7c5d7840aa25f5994856
Moss Green <gufeifei@outlook.com>
2016-07-28 19:54:15 +0800

    Finding affected areas in the code
    since we modified GigviewModel in previous movement, we have to modified
    corresponding parts in this project.
    
    In GigsController, we need to do the same thing.

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -43,11 +43,17 @@ namespace GigHub.Controllers
                 .Include(g => g.Genre)
                 .ToList();
 
+            var attendances = _context.Attendances
+               .Where(a => a.AttendeeId == userId && a.Gig.DateTime > DateTime.Now)
+               .ToList()
+               .ToLookup(a => a.GigId);
+
             var viewModel = new GigsViewModel()
             {
                 UpcomingGigs = gigs,
                 ShowActions = User.Identity.IsAuthenticated,
-                Heading = "Gigs I'm Attending"
+                Heading = "Gigs I'm Attending",
+                Attendances = attendances,
             };
 
             return View("Gigs", viewModel);
