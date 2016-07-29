74f69f24f3331cb255a27f2511d4dc06897b8222
Moss Green <gufeifei@outlook.com>
2016-07-29 18:40:19 +0800

    refactoring: Cleaning the Init Method

--- GigHub/Scripts/app/app.js
+++ GigHub/Scripts/app/app.js
@@ -5,39 +5,39 @@
                 with content of GigId, which comes from this button's attribute
                 it will be passed as an Attendance Dto to controllers/api/AttendanceController,
                 database will save this Attendance with current user's id and artist id.*/
-        $(".js-toggle-attendance").click(function (e) {
-            var button = $(e.target);
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };
 
-            if (button.hasClass("btn-default")) {
-                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                    .done(function () {
-                        button
-                            .removeClass("btn-default")
-                            .addClass("btn-info")
-                            .text("Going");
-                    })
-                    .fail(function () {
-                        alert("Something failed!");
-                    });
-            } else {
-                $.ajax({
-                    url: "/api/attendances/" + button.attr("data-gig-id"),
-                    method: "DELETE"
+    var toggleAttendance = function (e) {
+        var button = $(e.target);
 
-                }).done(function () {
+        if (button.hasClass("btn-default")) {
+            $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                .done(function () {
                     button
-                        .removeClass("btn-info")
-                    .addClass("btn-default")
-                    .text("Going?");
+                        .removeClass("btn-default")
+                        .addClass("btn-info")
+                        .text("Going");
                 })
                 .fail(function () {
-
-                    alert("btn-info failed to remove. ");
+                    alert("Something failed!");
                 });
-            }
+        } else {
+            $.ajax({
+                url: "/api/attendances/" + button.attr("data-gig-id"),
+                method: "DELETE"
 
-        });
+            }).done(function () {
+                button
+                    .removeClass("btn-info")
+                .addClass("btn-default")
+                .text("Going?");
+            })
+            .fail(function () {
 
+                alert("btn-info failed to remove. ");
+            });
+        }
     };
 
     return {
