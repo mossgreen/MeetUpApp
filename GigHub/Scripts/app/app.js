e9dc65c48cb73a609e2eb85658d16a8f2b9063a2
Moss Green <gufeifei@outlook.com>
2016-07-29 18:36:32 +0800

    refactoring using revealing module pattern

--- GigHub/Scripts/app/app.js
+++ GigHub/Scripts/app/app.js
@@ -1,7 +1,42 @@
 ﻿var GigsController = function () {
 
     var init = function () {
+        /*clicking the Going button will trager a http post
+                with content of GigId, which comes from this button's attribute
+                it will be passed as an Attendance Dto to controllers/api/AttendanceController,
+                database will save this Attendance with current user's id and artist id.*/
+        $(".js-toggle-attendance").click(function (e) {
+            var button = $(e.target);
 
+            if (button.hasClass("btn-default")) {
+                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                    .done(function () {
+                        button
+                            .removeClass("btn-default")
+                            .addClass("btn-info")
+                            .text("Going");
+                    })
+                    .fail(function () {
+                        alert("Something failed!");
+                    });
+            } else {
+                $.ajax({
+                    url: "/api/attendances/" + button.attr("data-gig-id"),
+                    method: "DELETE"
+
+                }).done(function () {
+                    button
+                        .removeClass("btn-info")
+                    .addClass("btn-default")
+                    .text("Going?");
+                })
+                .fail(function () {
+
+                    alert("btn-info failed to remove. ");
+                });
+            }
+
+        });
 
     };
 
@@ -12,41 +47,3 @@
 }();
 
 
-function initGigs() {
-    /*clicking the Going button will trager a http post
-                with content of GigId, which comes from this button's attribute
-                it will be passed as an Attendance Dto to controllers/api/AttendanceController,
-                database will save this Attendance with current user's id and artist id.*/
-    $(".js-toggle-attendance").click(function (e) {
-        var button = $(e.target);
-
-        if (button.hasClass("btn-default")) {
-            $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                .done(function () {
-                    button
-                        .removeClass("btn-default")
-                        .addClass("btn-info")
-                        .text("Going");
-                })
-                .fail(function () {
-                    alert("Something failed!");
-                });
-        } else {
-            $.ajax({
-                url: "/api/attendances/" + button.attr("data-gig-id"),
-                method: "DELETE"
-
-            }).done(function () {
-                button
-                    .removeClass("btn-info")
-                .addClass("btn-default")
-                .text("Going?");
-            })
-            .fail(function () {
-
-                alert("btn-info failed to remove. ");
-            });
-        }
-
-    });
-};
